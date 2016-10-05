46b60bed2b2778b33615e10739a97069bb1861b5
zhou <zhou.lv.wen@gmail.com>
2016-10-06 00:31:20 +0800

    update

--- main.m
+++ main.m
@@ -2,31 +2,20 @@
 %
 % Zhou Lvwen: zhou.lv.wen@gmail.com
 
-Lc = 22.05;               % m
-chain = 2;
-M = 1200;                 % kg;
-depth = 18;               % m
-
 % -------------------------------------------------------------------------
 % Question 1
 %
+Lc = 22.05; chain = 2; M = 1200; depth = 18;
 [tilt,elev,xsbed,xbuoy,f] = moor(Lc, chain, 12, 0, M, depth, 1);
 [tilt,elev,xsbed,xbuoy,f] = moor(Lc, chain, 24, 0, M, depth, 1);
 
 % -------------------------------------------------------------------------
 % Question 2
 %
-i = 0;
-Mi = [];
-tilti = [];
-elevi = [];
-while f<0.7
-    i = i + 1;
-    mi = 1200+10*i;
+Mi = 1200:10:4000; tilti = []; elevi = [];
+for mi = Mi
     [tilt,elev,xsbed,xbuoy,f] = moor(Lc,chain,36,0,mi, depth);
-    Mi = [Mi, mi];
-    tilti = [tilti, tilt];
-    elevi = [elevi, elev];
+    tilti = [tilti, tilt]; elevi = [elevi, elev];
 end
 
 figure('name','problem 2'); 
@@ -38,12 +27,13 @@ set(gca,'ytick',[0,5,10,16,20]); grid on
 legend('Tilt angle of the drum','Elevation angle of the chain at the anchor')
 xlabel('Mass of the heavy ball (kg)'); ylabel('Angle (degree)')
 
+idx = find( tilti<=5 & elevi<=16 );
+[tilt,elev,xbuoy,f] = moor(Lc, chain, 36, 0, min(Mi(idx)), depth, 1);
+
 % -------------------------------------------------------------------------
 % Question 3
-Lc = 22.05;               % m
-chain = 2;
-vw = 36;                  % m/s
-vs = 1.5;                 % m/s
-M = 3000;                 % kg
-depth = 20;               % m
-[tilt,elev,xsbed,xbuoy,f] = moor(Lc,chain,vw,vs,M,20,1);
+Lc = 15.84; chain = 5; vw = 36; vs = 1.5; M = 4020; depth = 16;
+[tilt,elev,xsbed,xbuoy,f] = moor(Lc,chain,vw,vs,M,depth,1);
+
+Lc = 20.88; chain = 5; vw = 36; vs = 1.5; M = 4000; depth = 20;
+[tilt,elev,xsbed,xbuoy,f] = moor(Lc,chain,vw,vs,M,depth,1);
